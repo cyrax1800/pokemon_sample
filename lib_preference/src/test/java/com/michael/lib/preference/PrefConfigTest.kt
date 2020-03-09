193454f21a0db6d7924286ab37ec5d74fb1aaed8
Michael <mchen0358@gmail.com>
2020-03-09 12:47:42 +0700

    add PrefConfigTest

--- lib_preference/src/test/java/com/michael/lib/preference/PrefConfigTest.kt
+++ lib_preference/src/test/java/com/michael/lib/preference/PrefConfigTest.kt
@@ -1,2 +1,23 @@
 package com.michael.lib.preference
 
+import android.content.Context
+import io.mockk.mockk
+import org.junit.Assert
+import org.junit.Test
+
+class PrefConfigTest {
+
+    val mockContext = mockk<Context>(relaxed = true)
+
+    @Test
+    fun `given configurator, when getContext, should return assigned context`() {
+        // given when
+        PrefConfig.init(object : PrefConfigContract {
+            override val context: Context
+                get() = mockContext
+        })
+
+        // then
+        Assert.assertEquals(PrefConfig.context, mockContext)
+    }
+}
